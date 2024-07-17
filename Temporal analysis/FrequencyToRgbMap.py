bug fix in VarianceToRGB.py: color space mapping error corrected. Variance range set to 0~10, max intensity projection range set to 0~10.

Temporal analysis/FrequencyToRgbMap.py
@@ -15,15 +15,15 @@ import matplotlib
 matplotlib.use("Qt5Agg")
 
 
-folderPath = r"F:\Data_2024\20240711_StabilityStudy\20240711_StabilityTest_YeastInRoom2Floor"
-stackname = "Storage_20240711_17h46m53s_IntImg"
+folderPath = r"F:\Data_2024\20240626_jurkat\hv-0hr"
+stackname = "Storage_20240626_12h06m20s_IntImg"
 stackFilePath = folderPath + "\\" + stackname + ".tif"
 rawData = tifffile.imread(stackFilePath)   # load linear intensity data from stack. Dimension (Y, Z, X)
 dim_y, dim_z, dim_x = np.shape(rawData)
 rawDataRotat = np.swapaxes(rawData, 0, 2)  # rotate en-face plane for 90 degree, so that fast scan (X) is horizontal axis to easy 2D FFT. Dimension (X, Z, Y)
 freqEncodeProj = np.zeros((dim_x, dim_z, 3), 'uint8')
 fs = 50  # Hz, B-scan frequency during acquisition
-band1 = 0.4 ;  band2 = 1.3 ; band3 = 5  # Hz, check figure(12); (0.75, 1.1, 2.2)
+band1 = 0.4 ;  band2 = 1.1 ; band3 = 3  # Hz, check figure(12); (0.75, 1.1, 2.2)
 band_discr = (np.divide([band1, band2, band3], fs) * dim_y).astype('int')
 
 for depthIndex in range(dim_z):   # dim_z
@@ -43,9 +43,9 @@ for depthIndex in range(dim_z):   # dim_z
     r = np.sum(ampEnfaceFreqSpec[:, 0:band_discr[0]], axis=1)
     g = np.sum(ampEnfaceFreqSpec[:, band_discr[0]:band_discr[1]], axis=1)
     b = np.sum(ampEnfaceFreqSpec[:, band_discr[1]:band_discr[2]], axis=1)
-    r_ch = (r+0) / 500  # [1:7] [7:17] [17:28] [28:]
-    g_ch = (g+0) / 500  # 0~0.75hz:10; 0.75~5hz:30; 5~25hz:40
-    b_ch = (b+0) / 1000
+    r_ch = (r+0) / 30  # [1:7] [7:17] [17:28] [28:]
+    g_ch = (g+0) / 50  # 0~0.75hz:10; 0.75~5hz:30; 5~25hz:40
+    b_ch = (b+0) / 50
     freqEncodeProj[:, depthIndex, 0] = r_ch*256;  freqEncodeProj[:, depthIndex, 1] = g_ch*256;  freqEncodeProj[:, depthIndex, 2] = b_ch*256
 
     print(str(depthIndex) + '/' + str(dim_z))

Temporal analysis/VarianceToRGB.py
@@ -20,7 +20,7 @@ matplotlib.use("Qt5Agg")
 
 
 folderPath = r"F:\Data_2024\20240711_StabilityStudy\20240711_StabilityTest_YeastInRoom2Floor"
-stackname = "Storage_20240711_17h46m53s_IntImg"
+stackname = "Storage_20240711_17h45m15s_IntImg"
 stackFilePath = folderPath + "\\" + stackname + ".tif"
 fs = 50  # Hz, B-scan frequency during acquisition
 cutoff = 2;  order = 2  # (cutoff frequency, filtering order), lowpass filter to remove DC component before computing variance
@@ -32,22 +32,22 @@ rawDataRotat = np.swapaxes(rawData, 0, 2)  # rotate en-face plane for 90 degree,
 varProj = np.zeros((dim_z, dim_x, 3), 'uint8')
 
 
-for depthIndex in range(650):   # dim_z
+for depthIndex in range(530):   # dim_z
     # depthIndex = 447  # # #
-    enfaceData = rawDataRotat[:, depthIndex, :]  # plt.figure(11);    plt.clf();    plt.plot(enfaceData[353, :])
+    enfaceData = rawDataRotat[:, depthIndex, :]  # # (dim_x, dim_y)
     enfaceData_lpfilt = butter_lowpass_filter(enfaceData, cutoff, fs, order)
     enfaceData_lp = enfaceData - enfaceData_lpfilt
     # plt.figure(11);    plt.clf();    plt.plot(enfaceData[355, :]);   plt.plot(enfaceData_lp[355, :])
 
-    hueRange = [0, 20]  # variance
+    hueRange = [0, 10]  # variance / std: 0~5
     satRange = [0, 10]  # intensity
-    varProj_var = np.var(enfaceData_lp, axis=1)
+    varProj_var = np.var(enfaceData_lp, axis=1)  # np.var()  /  np.std()
     varProj_hue = np.clip((varProj_var - hueRange[0]) / (hueRange[1] - hueRange[0]), 0, 1) * 1
     varProj_proj = np.max(enfaceData, axis=1)  # max projection along Y (time)
     varProj_sat = np.clip((varProj_proj - satRange[0]) / (satRange[1] - satRange[0]), 0, 1) * 1
-    varProj_val = np.ones((dim_x), 'uint8') * 250
+    varProj_val = np.ones((dim_x), 'uint8') * 1
     varProj_rgb = hsv_to_rgb(np.transpose([varProj_hue, varProj_sat, varProj_val]))
-    varProj[depthIndex, :, ] = varProj_rgb
+    varProj[depthIndex, :, :] = varProj_rgb * 255
 
     # varProj_rgb = num_to_rgb(np.var(enfaceData_lp, axis=1), max_val=20)
     # varProj_hsv = colorsys.rgb_to_hsv(varProj_rgb[0], varProj_rgb[1], varProj_rgb[2])
@@ -57,6 +57,6 @@ for depthIndex in range(650):   # dim_z
 
 img = varProj
 figsize_mag = 7
-plt.figure(16, figsize=(figsize_mag, dim_z/dim_x*figsize_mag));  plt.clf();  plt.imshow(img)
+plt.figure(16, figsize=(figsize_mag, dim_z/dim_x*figsize_mag));  plt.clf();  plt.imshow(img, vmin=0, vmax=255)
 plt.gca().set_axis_off(); plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
 gc.collect()