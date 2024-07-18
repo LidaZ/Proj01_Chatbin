6371668fa21ca501b5e23c26177e773f260a4e21
LidaZ <53167775+LidaZ@users.noreply.github.com>
2024-07-18 16:43:17 +0900

    Normalized speckle intensity variance is used for hue mapping. <10.1109/JTEHM.2019.2943317>
    Signed-off-by: LidaZ <53167775+LidaZ@users.noreply.github.com>

--- Temporal analysis/VarianceToRGB.py
+++ Temporal analysis/VarianceToRGB.py
@@ -35,7 +35,7 @@ dim_y, dim_z, dim_x = np.shape(rawData)
 rawDataRotat = np.swapaxes(rawData, 0, 2)  # rotate en-face plane for 90 degree, so that fast scan (X) is horizontal axis to easy 2D FFT. Dimension (X, Z, Y)
 varProj = np.zeros((dim_z, dim_x, 3), 'uint8')
 varProj_val = np.ones((dim_x), 'float32')
-hueRange = [0, 0.10]  # variance: 0~0.10 / std: 0~
+hueRange = [0, 0.05]  # variance: 0~0.05 / std: 0~
 satRange = [0, 0.3]  # intensity: 0~1
 
 for depthIndex in range(dim_z):   # dim_z
@@ -45,22 +45,21 @@ for depthIndex in range(dim_z):   # dim_z
     enfaceData_lp = enfaceData - enfaceData_lpfilt
     # plt.figure(11);    plt.clf();    plt.plot(enfaceData[355, :]);   plt.plot(enfaceData_lp[355, :])
 
-    varProj_var = np.var(enfaceData_lp, axis=1)  # np.var()  /  np.std()
-    varProj_hue = np.multiply(np.clip((varProj_var - hueRange[0]) / (hueRange[1] - hueRange[0]), 0, 1), 0.6)  # limit color display range from red to blue
     varProj_proj = np.max(enfaceData, axis=1)  # max projection along Y (time)
     varProj_sat = np.clip((varProj_proj - satRange[0]) / (satRange[1] - satRange[0]), 0, 1)
+    varProj_var = np.var(enfaceData_lp, axis=1)  # np.var()  /  np.std()
+    varProj_var_norm = np.divide(varProj_var, varProj_sat)  # normalized intensity to be robust to noise
+    varProj_hue = np.multiply(np.clip((varProj_var_norm - hueRange[0]) / (hueRange[1] - hueRange[0]), 0, 1), 0.6)  # limit color display range from red to blue
     varProj_rgb = hsv_to_rgb(np.transpose([varProj_hue, varProj_sat, varProj_val]))
     varProj[depthIndex, :, :] = varProj_rgb * 255
 
     # varProj_rgb = num_to_rgb(np.var(enfaceData_lp, axis=1), max_val=20)
     # varProj_hsv = colorsys.rgb_to_hsv(varProj_rgb[0], varProj_rgb[1], varProj_rgb[2])
     # varProj[depthIndex, :, ] = np.transpose(varProj_rgb)
-
     print(str(depthIndex) + '/' + str(dim_z))
 
-img = varProj
 figsize_mag = 7
-plt.figure(16, figsize=(figsize_mag, dim_z/dim_x*figsize_mag));  plt.clf();  plt.imshow(img, vmin=0, vmax=1)
+plt.figure(16, figsize=(figsize_mag, dim_z/dim_x*figsize_mag));  plt.clf();  plt.imshow(varProj, vmin=0, vmax=1)
 plt.gca().set_axis_off(); plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
 gc.collect()
 
