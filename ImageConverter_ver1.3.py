b6e1ec14f5de2248f6e9a01bbadf67f547e83290
LidaZ <53167775+LidaZ@users.noreply.github.com>
2024-07-18 10:29:16 +0900

    Bug fixed in TkInterface GUI function: error in event mainloop of system_select()roll back.
    Signed-off-by: LidaZ <53167775+LidaZ@users.noreply.github.com>

--- ImageConverter_ver1.3.py
+++ ImageConverter_ver1.3.py
@@ -12,34 +12,48 @@ from tkinter import filedialog
 matplotlib.use("Qt5Agg")
 
 
-sys_ivs800 = True  # Set as True if taken by IVS-800
-gpu_proc = True
+def return_datatype(dataid):
+    dtype = None
+    if dataid[-4:] == '.dat':  dtype = 'timelapse'
+    elif dataid[-4:] == '.bin':  dtype = '3d'
+    else: raise ValueError('Unrecognizable data type')
+    return dtype
+
+
+sys_ivs800 = False
 
 save_view = False  # Set as True if save dB-OCT img as 3D stack file for view
+save_tif = False  # Set as True if save intensity img as 3D stack .tiff file in the current folder
+
 save_video = False  # (Only for dtype='timelapse') set as True if save Int_view img as .mp4
 display_proc = False  # Set as True if monitor img during converting
+gpu_proc = True
 
-save_tif = True  # Set as True if save intensity img as 3D stack .tiff file in the current folder
+# # # - - - - - - popup box to select system type - - - - - - # # #
+# def sys_select(op):
+#     global sys_ivs800
+#     sys_type = variable.get()
+#     if sys_type == 'IVS-800': sys_ivs800 = True
+#     elif sys_type == 'IVS-2000-HR': sys_ivs800 = False
+#     else: ValueError('Wrong system type')
+#     tk_par.destroy()
+# choices = ['IVS-800', 'IVS-2000-HR']
+# tk_par = Tk();variable = StringVar(tk_par);variable.set(choices[0])
+# popupMenu = OptionMenu(tk_par, variable, *choices, command=sys_select);
+# Label(tk_par, text="Choose the used system").grid(row=0, column=0); popupMenu.grid(row=1, column=0)
+# tk_par.mainloop()
+
 if gpu_proc:
     import cupy as np; from cupyx.scipy.ndimage import zoom # import nvTIFF
 octRangedB = [-10, 70]  # set dynamic range of log OCT signal display
-if sys_ivs800:  octRangedB = [-25, 20]
-# root = r"F:\Data_2024\20240626_jurkat\lv-0hr"  # Folder path which containing the raw Data
-# DataId = "Storage_20240626_12h35m58s.dat"
-# DataFold = root + '\\' + DataId  # Raw data file name, usually it is Data.bin
-tk = Tk(); tk.withdraw(); DataFold = filedialog.askopenfilename(filetypes=[("", "*")])  # use TkInterface to catch dir path of data file
-DataId = os.path.basename(DataFold);   root = os.path.dirname(DataFold)
+if sys_ivs800:
+    octRangedB = [-25, 20]
+# # # - - - - use TkInterface to catch dir path of data file - - - - # # #
+tk = Tk(); tk.withdraw(); DataFold = filedialog.askopenfilename(filetypes=[("", "*")])
+DataId = os.path.basename(DataFold);   root = os.path.dirname(DataFold); tk.destroy()
 
-
-dataType = None
-[dim_y, dim_z, dim_x,FrameRate] = [0, 0, 0, 30]
-aspect_ratio = 1
-if DataId[-4:] == '.dat':
-    dataType = 'timelapse'
-elif DataId[-4:] == '.bin':
-    dataType = '3d'
-else:
-    raise ValueError('Unrecognizable data type')
+dataType = return_datatype(DataId)
+[dim_y, dim_z, dim_x, FrameRate] = [0, 0, 0, 30];  aspect_ratio = 1
 if dataType is not None:
     if dataType == 'timelapse':  # # # # # # # # # #
         with open(DataFold, mode='rb') as file:  # Read info from header
@@ -64,6 +78,9 @@ if save_view or save_tif:
 if save_video and dataType == 'timelapse':
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     out = cv2.VideoWriter(root + '\\' + DataId[:-4] + '_' + dataType + '_video.avi', fourcc, round(FrameRate), (res_dim_x, dim_z))
+if display_proc:
+    plt.figure(1, figsize=(dim_x/dim_z*aspect_ratio*7, 7))
+    plt.gca().set_axis_off(); plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
 
 # dim_y = 1
 for index in tqdm(range(dim_y)):
@@ -95,7 +112,7 @@ for index in tqdm(range(dim_y)):
         # c = cv2.waitKey(1)
         # if c == 27:  break
     if display_proc:
-        plt.figure(1, figsize=(dim_x/dim_z*aspect_ratio*5, 5)); plt.clf();
+        plt.figure(1); plt.clf();
         if dataType == 'timelapse':
             plt.text(0, -10, 'frame = ' + str(index+1) + ' / ' + str(dim_y) + ' ,  '
                      + 'time = ' + str(round(1/FrameRate*index, 2)) + ' s')
@@ -109,11 +126,12 @@ if save_view:
     if dataType == '3d': res_octImgVol = zoom(octImgVol, [aspect_ratio, 1, 1], order=1)
     elif dataType == 'timelapse': res_octImgVol = octImgVol
     octImgView = (np.clip((res_octImgVol - octRangedB[0]) / (octRangedB[1] - octRangedB[0]),0, 1) * 255).astype(dtype='uint8')
-    tifffile.imwrite(root + '\\' + DataId[:-4] + '_' + dataType + '_view.tif', np.rollaxis(octImgView[:, :, :], 0,1).get())
+    if gpu_proc: tifffile.imwrite(root + '\\' + DataId[:-4] + '_' + dataType + '_view.tif', np.rollaxis(octImgView[:, :, :], 0,1).get())
+    else: tifffile.imwrite(root + '\\' + DataId[:-4] + '_' + dataType + '_view.tif', np.rollaxis(octImgView[:, :, :], 0,1))
 if save_tif:
     res_octImgVol = np.power(10, octImgVol/10)  # convert to linear intensity (square of signal amplitude)
-    # if gpu_proc:
-    tifffile.imwrite(root + '\\' + DataId[:-4] + '_IntImg.tif', np.rollaxis(res_octImgVol[:, :, :], 0,1).get().astype(dtype='float32'))#, compression='zlib', compressionargs={'level': 8})
+    if gpu_proc: tifffile.imwrite(root + '\\' + DataId[:-4] + '_IntImg.tif', np.rollaxis(res_octImgVol[:, :, :], 0,1).get().astype(dtype='float32'))#, compression='zlib', compressionargs={'level': 8})
+    else: tifffile.imwrite(root + '\\' + DataId[:-4] + '_IntImg.tif', np.rollaxis(res_octImgVol[:, :, :], 0,1).astype(dtype='float32'))
 if save_video and dataType == 'timelapse':
     out.release();     cv2.destroyAllWindows()
 
