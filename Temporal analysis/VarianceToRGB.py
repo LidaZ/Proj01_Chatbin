Error in hsv to rgb function corrected: color display range limited from red to blue by multiplying a factor 0.6 to hue value.
Signed-off-by: LidaZ <53167775+LidaZ@users.noreply.github.com>

Temporal analysis/VarianceToRGB.py
@@ -24,7 +24,7 @@ matplotlib.use("Qt5Agg")
 # stackname = "Storage_20240626_13h15m17s_IntImg"  # Storage_20240626_13h24m16s_IntImg / Storage_20240626_13h15m17s_IntImg
 # stackFilePath = folderPath + "\\" + stackname + ".tif"
 tk = Tk(); tk.withdraw(); stackFilePath = filedialog.askopenfilename(filetypes=[("", "*")])
-DataId = os.path.basename(stackFilePath);  # root = os.path.dirname(DataFold)
+DataId = os.path.basename(stackFilePath);   root = os.path.dirname(DataId);  tk.destroy()
 
 fs = 50  # Hz, B-scan frequency during acquisition
 cutoff = 2;  order = 2  # (cutoff frequency, filtering order), lowpass filter to remove DC component before computing variance
@@ -35,7 +35,7 @@ dim_y, dim_z, dim_x = np.shape(rawData)
 rawDataRotat = np.swapaxes(rawData, 0, 2)  # rotate en-face plane for 90 degree, so that fast scan (X) is horizontal axis to easy 2D FFT. Dimension (X, Z, Y)
 varProj = np.zeros((dim_z, dim_x, 3), 'uint8')
 varProj_val = np.ones((dim_x), 'float32')
-hueRange = [0, 0.15]  # variance: 0~0.03 / std: 0~
+hueRange = [0, 0.10]  # variance: 0~0.10 / std: 0~
 satRange = [0, 0.3]  # intensity: 0~1
 
 for depthIndex in range(dim_z):   # dim_z
@@ -46,7 +46,7 @@ for depthIndex in range(dim_z):   # dim_z
     # plt.figure(11);    plt.clf();    plt.plot(enfaceData[355, :]);   plt.plot(enfaceData_lp[355, :])
 
     varProj_var = np.var(enfaceData_lp, axis=1)  # np.var()  /  np.std()
-    varProj_hue = np.clip((varProj_var - hueRange[0]) / (hueRange[1] - hueRange[0]), 0, 1)
+    varProj_hue = np.multiply(np.clip((varProj_var - hueRange[0]) / (hueRange[1] - hueRange[0]), 0, 1), 0.6)  # limit color display range from red to blue
     varProj_proj = np.max(enfaceData, axis=1)  # max projection along Y (time)
     varProj_sat = np.clip((varProj_proj - satRange[0]) / (satRange[1] - satRange[0]), 0, 1)
     varProj_rgb = hsv_to_rgb(np.transpose([varProj_hue, varProj_sat, varProj_val]))
@@ -63,3 +63,4 @@ figsize_mag = 7
 plt.figure(16, figsize=(figsize_mag, dim_z/dim_x*figsize_mag));  plt.clf();  plt.imshow(img, vmin=0, vmax=1)
 plt.gca().set_axis_off(); plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
 gc.collect()
+