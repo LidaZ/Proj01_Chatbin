Fix: error in computing variance from raw batch data is corrected, and the corresponding display min range is set to zero.

Temporal analysis/VarianceToRGB.py
@@ -24,7 +24,7 @@ matplotlib.use("Qt5Agg")
 sys_ivs800 = False
 rasterRepeat = 32
 errorShiftFrame = 0  # = 1 before 2024/09/05. Bug in scan pattern was fixed.
-saveImg = True
+saveImg = False
 
 
 tk = Tk(); tk.withdraw(); tk.attributes("-topmost", True); stackFilePath = filedialog.askopenfilename(filetypes=[("", "*_IntImg.tif")])
@@ -50,13 +50,13 @@ elif rasterRepeat == 1:
 batchList = np.linspace(0, dim_y, int(dim_y/rasterRepeat), endpoint=False)
 varRgbImg = np.zeros((dim_y_raster, dim_z, dim_x, 3), 'uint8')
 batchProj_sat = np.ones((dim_z, dim_x), 'float32')
-hueRange = [0.4, 1.0]  # variance: 0~0.15 / std: 0~0.3
+hueRange = [0., 1.0]  # variance: 0~0.15 / std: 0~0.3
 
 octRangedB = [0, 50]  # set dynamic range of log OCT signal display
 if sys_ivs800:  octRangedB = [-25, 20]
 
 
-# dim_y_raster = 10
+dim_y_raster = 10
 for batch_id in range(dim_y_raster):
     # # # - - - filt dc component, extract fluctuation with f>0.5hz when fs=50hz - - - # # #
     rawDat_batch = rawDat[(batch_id*rasterRepeat+errorShiftFrame):(batch_id+1)*rasterRepeat, :, :]  # [32(y), 300(z), 256(x)]
@@ -67,13 +67,14 @@ for batch_id in range(dim_y_raster):
     rawDat_batch_filt = rawDat_batch # - rawDat_batch_dc        # linear signal int
 
     # # # - - - compute max int at each pix as value - - - # # #
-    batchProj_valMax = np.log10( np.max(rawDat_batch_filt, axis=0) ) # Log int, not in dB (x10) yet
+    rawDat_batch_log = np.log10(rawDat_batch_filt)
+    batchProj_valMax = np.max(rawDat_batch_log, axis=0)  # Log int, not in dB (x10) yet
     # batchProj_sat = batchProj_valMax / np.max(batchProj_valMax)
     batchProj_val = np.clip((np.multiply(10, batchProj_valMax)-octRangedB[0]) / (octRangedB[1]-octRangedB[0]), 0, 1)   # clipped Log int in dB
     plt.figure(13); plt.clf(); plt.imshow(batchProj_val, cmap='gray')
 
     # # # - - - compute variance/std/freq at each pix - - - # # #
-    batchProj_var = np.var(batchProj_valMax, axis=0)  # np.var() / np.std(); # log int = batchProj_valMax, linear int = rawDat_batch_filt
+    batchProj_var = np.var(rawDat_batch_log, axis=0)  # np.var() / np.std(); # log int = batchProj_valMax, linear int = rawDat_batch_filt
     batchProj_varNorm = np.divide(batchProj_var, (batchProj_val+1))
 
     batchProj_varHue = np.multiply(np.clip(